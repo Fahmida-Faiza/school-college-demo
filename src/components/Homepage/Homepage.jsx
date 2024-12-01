3c82f1d51af8acb26cb09e35b3f950ed19156e8c
fahmida-faiza <fahmidafaiza57@gmail.com>
2024-12-01 21:14:36 +0600

    added swiper

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+"use client"
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Image from 'next/image';
 
 const Homepage = () => {
     return (
         <div>
-            <h1>this is homepage</h1>
+            <Swiper
+                cssMode={true}
+                navigation={true}
+                pagination={true}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                className="mySwiper"
+            >
+                <SwiperSlide><Image src="/slide-04.jpg" height="1000" width="4000" alt="" /></SwiperSlide>
+                <SwiperSlide><Image src="/slide-03.jpg" height="1000" width="4000" alt="" /></SwiperSlide>
+                <SwiperSlide><Image src="/slide-04.jpg" height="1000" width="4000" alt="" /></SwiperSlide>
+                <SwiperSlide><Image src="/slide-03.jpg" height="1000" width="4000" alt="" /></SwiperSlide>
+                <SwiperSlide><Image src="/slide-04.jpg" height="1000" width="4000" alt="" /></SwiperSlide>
+               
+            </Swiper>
         </div>
     );
 };
