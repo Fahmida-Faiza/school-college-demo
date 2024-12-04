f8e1241ef71c8821d37e6bd159caad053d5fe450
fahmida-faiza <fahmidafaiza57@gmail.com>
2024-12-04 23:03:24 +0600

    added details

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -2,7 +2,7 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -14,12 +14,17 @@ const Homepage = () => {
     return (
         <>
             <Swiper
-                cssMode={true}
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
                 navigation={true}
-                pagination={true}
-                mousewheel={true}
-                keyboard={true}
-                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
                 <SwiperSlide><Image src="/slide-04.jpg" height="1000" width="4000" alt="" /></SwiperSlide>
@@ -41,13 +46,13 @@ const Homepage = () => {
             <div className="hero text-black border  my-14">
                 <div className="hero-content flex-col lg:flex-row">
                     <Image
-                        src="/card.png"
-                        className="rounded-lg shadow-2xl w-[50%]" alt="" height="1000" width="4000" />
+                        src="/9187604.png"
+                        className="rounded-lg shadow-2xl w-[30%]" alt="" height="1000" width="4000" />
                     <div>
                         <h1 className="text-5xl font-bold">Message Of Principal</h1>
                         <p className="py-6">
 
-                            বাংলাদেশ বিমান বাহিনী কর্তৃক পরিচালিত দেশের ঐতিহ্যবাহী এবং স্বনামধ্য শিক্ষা প্রতিষ্ঠান বিএএফ শাহীন কলেজ কুর্মিটোলা ঢাকা সেনানিবাসের এক মনোরম এবং সুনির্মল পরিবেশে অবস্থিত। একটি আদর্শ শিক্ষাঙ্গন হিসাবে ইতোমধ্যে এই কলেজটি দেশব্যাপী ব্যাপক পরিচিতি লাভ করেছে। নগর জীবনের কোলাহল এবং যানজট মুক্ত, অরাজনৈতিক  সম্পূর্ন নিরাপদ এক অনন্য শিক্ষা প্রতিষ্ঠানের নাম বিএএফ শাহীন কলেজ কুর্মিটোলা। ১৯৭২ সালে  “এয়ারফোর্স স্কুল” নামে এই প্রতিষ্ঠানটির পথচলা শুরু হয় এবং কালের পরিক্রমায় এটি সাফল্যের শীর্ষ সীমায় পৌছে যায়। সীমিত পরিসরে এর যাত্রা শুরু হলেও ধীরে ধীরে কলেবর বৃদ্ধি পেয়ে এটি এখন এক অনন্য উচ্চতার পূর্ণাঙ্গ শিক্ষা প্রতিষ্ঠান।  ১৯৮০ সালে এটির নামকরন করা হয় “বিএএফ শাহীন স্কুল”। ১৯৮২ সালে এটিকে দ্বাদশ শ্রেণি পর্যন্ত উন্নীত করা হয় এবং পুনরায় নামকরন করা হয় “বিএএফ শাহীন কলেজ কুর্মিটোলা”। শিশু থেকে দ্বাদশ শ্রেণি পর্যন্ত বাংলা ও ইংরেজি ভার্সনে পাঠদান করা হয় এই শিক্ষা প্রতিষ্ঠানে।
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis in expedita, vel dolore vitae illo libero cum voluptate necessitatibus id assumenda deleniti quae nulla nihil!
 
                         </p>
 
@@ -58,7 +63,7 @@ const Homepage = () => {
 
             {/* 3rd */}
 
-            <div className="grid grid-cols-2 gap-4 w-[90%] mx-auto my-10">
+            <div className="grid grid-cols-2 gap-4 w-[90%] mx-auto my-20">
                 <div className="">
                     <div className="card shadow-xl">
                         <div className="card-body">
@@ -98,6 +103,7 @@ const Homepage = () => {
                             </ol>
 
                         </div>
+                        <button className='text-blue-500 text-end px-5 btn-link'>All Notice</button>
                     </div>
                 </div>
 
@@ -107,74 +113,111 @@ const Homepage = () => {
 
 
             {/* 4th */}
-            <div className='grid grid-cols-4 gap-4 w-[90%] mx-auto'>
-                <div className="card w-80 text-black shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                            alt="Shoes"
-                            className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
 
-                <div className="card w-80 text-black shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                            alt="Shoes"
-                            className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="card w-80 text-black shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                            alt="Shoes"
-                            className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="card w-80 text-black shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                            alt="Shoes"
-                            className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
+            <div >
+                <h1 className='text-center my-5 font-semibold text-3xl text-black'>Teachers Details</h1>
             </div>
-
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={20}
+                grabCursor={true}
+                loop={true}
+                autoplay={{
+                    delay: 1000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper w-[80%] mx-auto   mt-30"
+            >
+                <SwiperSlide>
+                    <div className="card w-80 md:w-[22rem] text-black shadow-xl ">
+                        <figure className="px-10 pt-10">
+                        <Image src="/9187604.png" height="1000" width="4000" alt="" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card w-80 md:w-[22rem] text-black shadow-xl">
+                        <figure className="px-10 pt-10">
+                        <Image src="/9187604.png" height="1000" width="4000" alt="" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card w-80 md:w-[22rem] text-black shadow-xl">
+                        <figure className="px-10 pt-10">
+                        <Image src="/9187604.png" height="1000" width="4000" alt="" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card w-80 md:w-[22rem] text-black shadow-xl">
+                        <figure className="px-10 pt-10">
+                        <Image src="/9187604.png" height="1000" width="4000" alt="" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card w-80 md:w-[22rem] text-black shadow-xl">
+                        <figure className="px-10 pt-10">
+                        <Image src="/9187604.png" height="1000" width="4000" alt="" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card w-80 md:w-[22rem] text-black shadow-xl">
+                        <figure className="px-10 pt-10">
+                        <Image src="/9187604.png" height="1000" width="4000" alt="" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+          
 
 
 
